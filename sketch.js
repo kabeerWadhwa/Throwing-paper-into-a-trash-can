33af37850161da1694122635f5d6b9e3d61c9868
kabeerWadhwa <68274419+kabeerWadhwa@users.noreply.github.com>
2020-08-18 15:38:31 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,55 +1,43 @@
-
+var dustbinIMG
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-class Paper{
-	options()
-}
-
-var options = {
-		isStatic:false,
-		restitution:0.3,
-		friction:0.5,
-		density:1.2
-	}
-
-var dustbinSide1=createSprite(700,645,100,20);
-var dustbinSide2=createSprite(750,645,100,20);
-var dustbinSide3=createSprite(725,645,20,100);
-
-class ground{
-	groundOptions()
-}
-
-var ground=createSprite(400,650,20,100);
-var groundOptions = {
-		isStatic:true,
-}
+var engine,world
 function preload()
 {
-	
+dustbinIMG = loadImage("Sprites/dustbingreen.png")
 }
 
 function setup() {
-	createCanvas(800, 700);
-
+	createCanvas(800,700);
 
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
-
-
+	//Create the Bodies Here
+	//var dustbin1= createSprite(610,580,20,100);
+	//var dustbin2= createSprite(650,630,100,20);
+	var dustbin3= createSprite(690,580,20,100);
+	dustbin3.addImage(dustbinIMG)
+	dustbin3.scale = 0.5
+	ground1 = new ground(400,650,800,20);
+	paper1 = new Paper(200,650,20,20);
 	Engine.run(engine);
-  
 }
-
 
 function draw() {
   rectMode(CENTER);
-  background(0);
-  
+  background(255,255,255);
+  Engine.update(engine)
+  ground1.display()
+  paper1.display()
   drawSprites();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85, y:-85})
+	}
 }
